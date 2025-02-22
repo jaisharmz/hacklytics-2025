6ccfe7c6f74922544f5ddc92f659bf9a76e38891
jaisharmz <jaisharmaus@gmail.com>
2025-02-22 00:44:25 -0500

    eda

--- eda.py
+++ eda.py
@@ -10,8 +10,11 @@ from constants import IMPORTANT_TICKERS_36, SYP_500, RUSSELL_1000
 # interesting_tickers = ["MSFT", "AAPL", "GOOG", "NVDA", "AMD", "TSLA"]
 interesting_tickers = SYP_500
 df = yf.download(interesting_tickers, period='5y')
+df = df / df.iloc[0]
 print(df.shape)
 
-open_columns = [("Open", ticker) for ticker in interesting_tickers]
+open_columns = [("Open", ticker) for ticker in interesting_tickers][:10]
 df.loc[:,open_columns].plot()
+plt.show()
+df.loc[:,open_columns].rolling(10).mean().plot()
 plt.show()
